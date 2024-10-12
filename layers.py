26d178be54c8249792981a5789be6cd0fb5222f6
dadukhankevin <danieljlosey@gmail.com>
2024-10-12 15:35:27 -0500

    better mutation

--- layers.py
+++ layers.py
@@ -70,6 +70,7 @@ class NPointCrossover(Layer):
             children = self.parent(parents)
             new_individuals.extend(children)
         return new_individuals
+
     
 class UniformMutation(Layer):
     def __init__(self, selection_function: Callable, device: str = 'cpu', magnitude: float = 0.1):
@@ -91,10 +92,57 @@ class UniformMutation(Layer):
         
         mutated_genes = individual.genes + noise
 
-
         # Clip values between 0 and 1
         mutated_genes = np.clip(mutated_genes, 0, 1)
 
         individual.genes = mutated_genes
+        self.mark_modified(individual)
+        return individual
+
+class BinaryFlipMutation(Layer):
+    def __init__(self, selection_function: Callable, flip_rate: float = 0.1):
+        super().__init__()
+        self.selection_function = selection_function
+        self.flip_rate = flip_rate
+
+    def execute(self):
+        new_individuals = []
+        parents = self.selection_function(self.env.individuals)
+        for parent in parents:
+            mutated_individual = self.mutate(parent)
+            new_individuals.append(mutated_individual)
+        return new_individuals
+
+    def mutate(self, individual: Individual) -> Individual:
+        flip_mask = np.random.random(individual.genes.shape) < self.flip_rate
+        mutated_genes = np.where(flip_mask, 1 - individual.genes, individual.genes)
+
+        individual.genes = mutated_genes
+        self.mark_modified(individual)
+        return individual
+
+class SwapMutation(Layer):
+    def __init__(self, selection_function: Callable, swap_rate: float = 0.1):
+        super().__init__()
+        self.selection_function = selection_function
+        self.swap_rate = swap_rate
+
+    def execute(self):
+        new_individuals = []
+        parents = self.selection_function(self.env.individuals)
+        for parent in parents:
+            mutated_individual = self.mutate(parent)
+            new_individuals.append(mutated_individual)
+        return new_individuals
+
+    def mutate(self, individual: Individual) -> Individual:
+        genes = individual.genes.copy()
+        num_swaps = int(len(genes) * self.swap_rate)
+        
+        for _ in range(num_swaps):
+            idx1, idx2 = np.random.choice(len(genes), 2, replace=False)
+            genes[idx1], genes[idx2] = genes[idx2], genes[idx1]
+
+        individual.genes = genes
         self.mark_modified(individual)
         return individual
