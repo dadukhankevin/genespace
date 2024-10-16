76fee04d2b4e3260b74b18c1113b0e391ff9de6f
dadukhankevin <danieljlosey@gmail.com>
2024-10-15 23:28:59 -0500

    scale

--- general_evolution.py
+++ general_evolution.py
@@ -3,12 +3,6 @@ from genespace import layers, genepool, decoders, selection, environments
 import enum
 
 
-
-class Landscape(enum.Enum):
-    TINY = 1  # Many solutions
-    MEDIUM = 2  # Only one valid solution
-    LARGE = 3
-
 class GeneralEvolution:
     def __init__(self, fitness_function, output_shape: tuple[int, ...], scale: float=1, input_length: int = 250, device: str = "cpu"):
 
@@ -18,13 +12,13 @@ class GeneralEvolution:
         Also, adding layers almost always hurts.
         """
         self.scale = scale
-        self.hidden_size = 2000 * (self.scale ** 2)
+        self.hidden_size = int(2000 * (self.scale ** 2))
         print("hidden size: ", self.hidden_size)
         self.learn_rate = 0.00001
-        self.max_population = 200 * self.scale
+        self.max_population = int(200 * self.scale)
         
         self.decoder = decoders.MLPGeneSpaceDecoder(input_length=input_length, hidden_size=self.hidden_size, num_layers=1, output_shape=output_shape, lr=self.learn_rate, device=device)
-        self.genepool = genepool.GenePool(size=input_length*self.scale, gsp=self.decoder, binary_mode=True)
+        self.genepool = genepool.GenePool(size=input_length, gsp=self.decoder, binary_mode=True)
         self.num_families = int(max(16, self.max_population // 28)) 
         print("families: ", self.num_families)
         self.num_children = 4
@@ -36,7 +30,7 @@ class GeneralEvolution:
 
     def solve(self, generations=300, backprop_mode=environments.BackpropMode.GRADIENT_DESCENT, backprop_every_n=10, epochs=1, selection_percent=.4, batch_size=32):
         self.environment.compile(start_population=self.max_population, max_individuals=self.max_population)
-        self.environment.evolve(generations=generations, backprop_mode=backprop_mode, backprop_every_n=backprop_every_n * self.scale, epochs=epochs, selection_percent=selection_percent, batch_size=batch_size * self.scale)
+        self.environment.evolve(generations=generations, backprop_mode=backprop_mode, backprop_every_n=int(backprop_every_n * self.scale), epochs=epochs, selection_percent=selection_percent, batch_size=int(batch_size * self.scale))
     
     def plot_fitness(self):
         self.environment.plot()
